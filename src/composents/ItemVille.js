a99880550f377f863ecb46ca8809338a9880b19f
Mbayesarr <48923130+Mbayesarr@users.noreply.github.com>
2021-10-08 16:48:21 +0100

    Composant Ville-App

---
+++ src/composents/ItemVille.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ItemVille = () => {
+    return (
+        <div>
+           <li className="list-group-item m-2 border d-flex justify-content-between align-items-center">
+                    <h1 className="bottom-50 end-50">RABAT</h1>
+                    <button className="btn btn-danger">
+                        <i className="fa fa-trash" aria-hidden="true" />
+                    </button>
+                </li>
+        </div>
+    )
+}
+
+export default ItemVille
---
+++ src/composents/ListV.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ItemVille from './ItemVille'
+
+const ListV = () => {
+    return (
+        <div>
+             <h1 className="text-center">VILLE APP</h1>
+             <input type="text" placeholder="Ajouter une ville" className="form-control w-50 mx-auto m-3" />
+            <button className="text-center">Ajouter</button>
+            
+            <div className="filter">
+                <input type="text" placeholder="filter task by title" className="form-control w-50 mx-auto m-3" />
+            </div>
+            <ul className="list-group">
+                   <ItemVille/>
+            </ul>
+        </div>
+    )
+}
+
+export default ListV
